06b4ba5816d2f2b1385bd5fb5441e78189252107
jvaldesgonzalez <jvaldesgonzalez9@gmail.com>
2021-04-16 10:23:06 -0400

    feat(core): add FP-like monads and core interfaces like Entity and Aggregate

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = 3000;
+  await app.listen(port);
+  Logger.log(`🚀 Server running on port :${port}`, 'NestApplication');
 }
 bootstrap();
