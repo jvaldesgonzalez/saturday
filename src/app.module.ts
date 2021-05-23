95b91311c35c59d748d71939f32cbfbe0c82cabe
jvaldesgonzalez <jvaldesgonzalez9@gmail.com>
2021-05-22 23:10:34 -0400

    feat(app): add events and stories modules to app-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { EventsModule } from './modules/events/events.module';
+import { StoriesModule } from './modules/stories/stories.module';
 import { UsersModule } from './modules/users/users.module';
 import { DataAccessModule } from './shared/modules/data-access/data-access.module';
 
 @Module({
-  imports: [DataAccessModule, UsersModule],
+  imports: [DataAccessModule, UsersModule, EventsModule, StoriesModule],
 })
 export class AppModule {}
