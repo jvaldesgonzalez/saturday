5ed142e4b4998cc78c96ed457711f090b04f943a
jvaldesgonzalez <jvaldesgonzalez9@gmail.com>
2021-07-17 14:34:12 -0400

    refactor(abstract-entities): add IdEntity class for entities with ID behavior (Ex:UserRef)

---
+++ src/shared/domain/id-entity.ts
@@ -0,0 +1,18 @@
+import { Ok, Result } from 'src/shared/core/Result';
+import { DomainEntity } from 'src/shared/domain/entity.abstract';
+import { UniqueEntityID } from 'src/shared/domain/UniqueEntityID';
+import { Empty } from 'src/shared/typedefs/empty';
+
+export class IdEntity extends DomainEntity<Empty> {
+  get id(): UniqueEntityID {
+    return this.id;
+  }
+
+  private constructor(id?: UniqueEntityID) {
+    super(null, id);
+  }
+
+  public static create(id?: string): Result<IdEntity> {
+    return Ok(new IdEntity(new UniqueEntityID(id)));
+  }
+}
