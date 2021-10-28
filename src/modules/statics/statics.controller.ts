5907bf3abc137c19c7c527c7b382daa3c723d838
jvaldesgonzalez <jvaldesgonzalez9@gmail.com>
2021-10-28 02:26:58 -0400

    commit

--- src/modules/statics/statics.controller.ts
+++ src/modules/statics/statics.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
+import { ApiQuery, ApiTags } from '@nestjs/swagger';
 import { StaticsService } from './statics.service';
 
 @ApiTags('statics')
@@ -8,9 +8,15 @@ export class StaticsController {
   constructor(private service: StaticsService) {}
 
   @Get('/signed-urls/profiles/me')
-  async getSignedUrl() {
+  async getProfileSignedUrl() {
     return await this.service.getSignedUrl(
       `profile-777cc88c-2e3f-4eb4-ac81-14c9323c541d`,
     );
+  }
+
+  @Get('/signed-urls/events/:id')
+  @ApiQuery({ name: 'id' })
+  async getEventSignedUrl(@Param('id', ParseUUIDPipe) id: string) {
+    return await this.service.getSignedUrl(`event-${id}`);
   }
 }
