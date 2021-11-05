b09f2d65856631bc0bb77183c11f625049a8cd23
jvaldesgonzalez <jvaldesgonzalez9@gmail.com>
2021-11-04 23:56:58 -0400

    restore auth

--- src/modules/accounts-management/auth/guards/auth.guard.ts
+++ src/modules/accounts-management/auth/guards/auth.guard.ts
@@ -14,7 +14,6 @@ export class JwtAuthGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    return true;
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
       context.getClass(),
