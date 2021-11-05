90e2b6811d924f38ebbb687cff45558d8061d7a2
jvaldesgonzalez <jvaldesgonzalez9@gmail.com>
2021-11-05 00:51:33 -0400

    restore auth

--- src/modules/accounts-management/auth/jwt-utils/index.ts
+++ src/modules/accounts-management/auth/jwt-utils/index.ts
@@ -5,7 +5,7 @@ import * as crypto from 'crypto';
 
 export namespace JWTUtils {
   export function sign(claim: JWTClaim) {
-    return jwt.sign(claim, jwtSecret, { expiresIn: '3h' });
+    return jwt.sign(claim, jwtSecret, { expiresIn: '30s' });
   }
 
   export function decode(token: JWTToken): JWTClaim {
