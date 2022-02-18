fa875e8efc88ed0a85e00b77e1b8f76a94278b93
jvaldesgonzalez <jvaldesgonzalez9@gmail.com>
2022-02-18 16:37:29 -0500

    commit

--- src/modules/publications/publications.read-service.ts
+++ src/modules/publications/publications.read-service.ts
@@ -24,129 +24,198 @@ export class PublicationsReadService {
       QuerySpecification.withStatement(
         `
 				MATCH (p:Publication)
-				WHERE p.dateTimeEnd > datetime()
-				CALL apoc.when(p:Event,'
-					MATCH (pl:Place)<-[:HAS_PLACE]-(item)<-[:PUBLISH_EVENT]-(p:Partner),
-					(item)-[:HAS_CATEGORY]->(cat:Category),
-					(item)-[:HAS_OCCURRENCE]->(o:EventOccurrence)-[:HAS_TICKET]->(t:Ticket)
-					WHERE o.dateTimeEnd > datetime()
-					OPTIONAL MATCH (item)-[:HAS_TAG]-(tag:AttentionTag)
-					OPTIONAL MATCH (item)<-[:COLLABORATOR]-(c:Partner)
-					MATCH (me:User)
-					WHERE me.id = $meId
-					OPTIONAL MATCH (me)-[rfollow:FOLLOW]->(p)
-					OPTIONAL MATCH (me)-[rlike:LIKE]-(item)
-					OPTIONAL MATCH (u:User)-[:LIKE]->(item)
-					WITH {
-						id:o.id,
-						dateTimeInit:o.dateTimeInit,
-						dateTimeEnd:o.dateTimeEnd,
-						tickets:collect(distinct t { .id, .price, .name, .amount, .description})
-					} as occ, item as e, collect(distinct tag { .title, .color, .description}) as tags, p, pl, cat, collect(distinct c {.id,.avatar,.username}) as coll,count(distinct u) as usersInterested, rlike,rfollow,me
-					with distinct {
-						type:"event",
-						id:e.id,
-						name:e.name,
-						occurrences:collect(occ),
-						info:e.description,
-						publisher:{
-							id:p.id,
-							avatar:p.avatar,
-							username:p.username,
-							businessName:p.businessName,
-							IFollowThis: rfollow IS NOT null
-						},
-						category:{
-							name:cat.name,
-							id:cat.id
-						},
-						place:{
-							name:pl.name,
-							address:pl.address,
-							longitude:pl.longitude,
-							latitude:pl.latitude
-						},
-						collaborators: coll,
-						multimedia:e.multimedia,
-						attentionTags: tags,
-						amIInterested: rlike IS NOT null,
-						totalUsersInterested: usersInterested,
-						dateTimeInit:e.dateTimeInit,
-						dateTimeEnd:e.dateTimeEnd,
-						createdAt:e.createdAt,
-						basePrice:e.basePrice
-					} as eventInfo, me, e
-					call {
-						WITH e,me
-						OPTIONAL MATCH (me)-[:FRIEND]-(f:User)-[:LIKE]->(e)
-						RETURN f LIMIT 3
-          }
-					return apoc.map.merge(eventInfo, {friends:collect( distinct f{.username, .avatar})}) as result
-				','
-					MATCH (item)--(e:Event)
-					,(pl:Place)<-[:HAS_PLACE]-(e)<-[:PUBLISH_EVENT]-(p:Partner),
-					(e)-[:HAS_CATEGORY]->(cat:Category),
-					(e)-[:HAS_OCCURRENCE]->(o:EventOccurrence)-[:HAS_TICKET]->(t:Ticket)
-					WHERE o.dateTimeEnd > datetime() AND e.dateTimeEnd > datetime()
-					OPTIONAL MATCH (e)-[:HAS_TAG]-(tag:AttentionTag)
-					OPTIONAL MATCH (e)<-[:COLLABORATOR]-(c:Partner)
-					MATCH (me:User)
-					WHERE me.id = $meId
-					OPTIONAL MATCH (me)-[rfollow:FOLLOW]->(p)
-					OPTIONAL MATCH (me)-[rlike:LIKE]-(e)
-					OPTIONAL MATCH (u:User)-[:LIKE]->(e)
-					WITH {
-						id:o.id,
-						dateTimeInit:o.dateTimeInit,
-						dateTimeEnd:o.dateTimeEnd,
-						tickets:collect(distinct t { .id, .price, .name, .amount, .description})
-					} as occ, e, collect(distinct tag { .title, .color, .description}) as tags, p, pl, cat, collect(distinct c {.id,.avatar,.username}) as coll,count(distinct u) as usersInterested, rlike,rfollow,me,item
-					with distinct {
-						id:e.id,
-						name:e.name,
-						occurrences:collect(occ),
-						info:e.description,
-						publisher:{
-							id:p.id,
-							avatar:p.avatar,
-							username:p.username,
-							businessName:p.businessName,
-							IFollowThis: rfollow IS NOT null
-						},
-						category:{
-							name:cat.name,
-							id:cat.id
-						},
-						place:{
-							name:pl.name,
-							address:pl.address,
-							longitude:(pl.longitude),
-							latitude:(pl.latitude)
-						},
-						collaborators: coll,
-						multimedia:e.multimedia,
-						attentionTags: tags,
-						amIInterested: rlike IS NOT null,
-						totalUsersInterested: usersInterested,
-						dateTimeInit:e.dateTimeInit,
-						dateTimeEnd:e.dateTimeEnd,
-						basePrice:e.basePrice
-					} as eventInfo, me, e,item
-					call {
-						WITH e,me
-						OPTIONAL MATCH (me)-[:FRIEND]-(f:User)-[:LIKE]->(e)
-						RETURN f LIMIT 3
-          }
-					WITH apoc.map.merge(eventInfo, {friends:collect( distinct f{.username, .avatar})}) as events,item
-					return {
-     				type:"collection",
-						id:item.id,
-						name:item.name,
-						description:item.description,
-						events:collect(events),
-						createdAt:item.createdAt
-					} as result
-				',{item:p,meId:$meId}) YIELD value
+				WHERE (p:Event AND p.dateTimeEnd > datetime())
+				OR (p:Collection AND apoc.coll.max( [ (p)--(e:Event) | e.dateTimeEnd ] )> datetime() AND size( [ (p)--(e:Event) | e ] ) > 2)
+				OR (p:EmbeddedCollection AND apoc.coll.max( [ (p)--(e:Event) | e.dateTimeEnd ] )> datetime() AND size( [ (p)--(e:Event) | e ] ) > 2))
+				CALL apoc.case([
+					p:Event,
+						'MATCH (pl:Place)<-[:HAS_PLACE]-(item)<-[:PUBLISH_EVENT]-(p:Partner),
+						(item)-[:HAS_CATEGORY]->(cat:Category),
+						(item)-[:HAS_OCCURRENCE]->(o:EventOccurrence)-[:HAS_TICKET]->(t:Ticket)
+						WHERE o.dateTimeEnd > datetime()
+						OPTIONAL MATCH (item)-[:HAS_TAG]-(tag:AttentionTag)
+						OPTIONAL MATCH (item)<-[:COLLABORATOR]-(c:Partner)
+						MATCH (me:User)
+						WHERE me.id = $meId
+						OPTIONAL MATCH (me)-[rfollow:FOLLOW]->(p)
+						OPTIONAL MATCH (me)-[rlike:LIKE]-(item)
+						OPTIONAL MATCH (u:User)-[:LIKE]->(item)
+						WITH {
+							id:o.id,
+							dateTimeInit:o.dateTimeInit,
+							dateTimeEnd:o.dateTimeEnd,
+							tickets:collect(distinct t { .id, .price, .name, .amount, .description})
+						} as occ, item as e, collect(distinct tag { .title, .color, .description}) as tags, p, pl, cat, collect(distinct c {.id,.avatar,.username}) as coll,count(distinct u) as usersInterested, rlike,rfollow,me
+						with distinct {
+							type:"event",
+							id:e.id,
+							name:e.name,
+							occurrences:collect(occ),
+							info:e.description,
+							publisher:{
+								id:p.id,
+								avatar:p.avatar,
+								username:p.username,
+								businessName:p.businessName,
+								IFollowThis: rfollow IS NOT null
+							},
+							category:{
+								name:cat.name,
+								id:cat.id
+							},
+							place:{
+								name:pl.name,
+								address:pl.address,
+								longitude:pl.longitude,
+								latitude:pl.latitude
+							},
+							collaborators: coll,
+							multimedia:e.multimedia,
+							attentionTags: tags,
+							amIInterested: rlike IS NOT null,
+							totalUsersInterested: usersInterested,
+							dateTimeInit:e.dateTimeInit,
+							dateTimeEnd:e.dateTimeEnd,
+							createdAt:e.createdAt,
+							basePrice:e.basePrice
+						} as eventInfo, me, e
+						call {
+							WITH e,me
+							OPTIONAL MATCH (me)-[:FRIEND]-(f:User)-[:LIKE]->(e)
+							RETURN f LIMIT 3
+						}
+						return apoc.map.merge(eventInfo, {friends:collect( distinct f{.username, .avatar})}) as result',
+					p:Collection,
+						'MATCH (item)--(e:Event)
+						,(pl:Place)<-[:HAS_PLACE]-(e)<-[:PUBLISH_EVENT]-(p:Partner),
+						(e)-[:HAS_CATEGORY]->(cat:Category),
+						(e)-[:HAS_OCCURRENCE]->(o:EventOccurrence)-[:HAS_TICKET]->(t:Ticket)
+						WHERE o.dateTimeEnd > datetime() AND e.dateTimeEnd > datetime()
+						OPTIONAL MATCH (e)-[:HAS_TAG]-(tag:AttentionTag)
+						OPTIONAL MATCH (e)<-[:COLLABORATOR]-(c:Partner)
+						MATCH (me:User)
+						WHERE me.id = $meId
+						OPTIONAL MATCH (me)-[rfollow:FOLLOW]->(p)
+						OPTIONAL MATCH (me)-[rlike:LIKE]-(e)
+						OPTIONAL MATCH (u:User)-[:LIKE]->(e)
+						WITH {
+							id:o.id,
+							dateTimeInit:o.dateTimeInit,
+							dateTimeEnd:o.dateTimeEnd,
+							tickets:collect(distinct t { .id, .price, .name, .amount, .description})
+						} as occ, e, collect(distinct tag { .title, .color, .description}) as tags, p, pl, cat, collect(distinct c {.id,.avatar,.username}) as coll,count(distinct u) as usersInterested, rlike,rfollow,me,item
+						with distinct {
+							id:e.id,
+							name:e.name,
+							occurrences:collect(occ),
+							info:e.description,
+							publisher:{
+								id:p.id,
+								avatar:p.avatar,
+								username:p.username,
+								businessName:p.businessName,
+								IFollowThis: rfollow IS NOT null
+							},
+							category:{
+								name:cat.name,
+								id:cat.id
+							},
+							place:{
+								name:pl.name,
+								address:pl.address,
+								longitude:(pl.longitude),
+								latitude:(pl.latitude)
+							},
+							collaborators: coll,
+							multimedia:e.multimedia,
+							attentionTags: tags,
+							amIInterested: rlike IS NOT null,
+							totalUsersInterested: usersInterested,
+							dateTimeInit:e.dateTimeInit,
+							dateTimeEnd:e.dateTimeEnd,
+							basePrice:e.basePrice
+						} as eventInfo, me, e,item
+						call {
+							WITH e,me
+							OPTIONAL MATCH (me)-[:FRIEND]-(f:User)-[:LIKE]->(e)
+							RETURN f LIMIT 3
+						}
+						WITH apoc.map.merge(eventInfo, {friends:collect( distinct f{.username, .avatar})}) as events,item
+						return {
+							type:"collection",
+							id:item.id,
+							name:item.name,
+							description:item.description,
+							events:collect(events),
+							createdAt:item.createdAt
+						} as result',
+					p:EmbeddedCollection,
+						'MATCH (item)--(cat:Category)--(e:Event)
+						,(pl:Place)<-[:HAS_PLACE]-(e)<-[:PUBLISH_EVENT]-(p:Partner),
+						(e)-[:HAS_OCCURRENCE]->(o:EventOccurrence)-[:HAS_TICKET]->(t:Ticket)
+						WHERE o.dateTimeEnd > datetime() AND e.dateTimeEnd > datetime()
+						OPTIONAL MATCH (e)-[:HAS_TAG]-(tag:AttentionTag)
+						OPTIONAL MATCH (e)<-[:COLLABORATOR]-(c:Partner)
+						MATCH (me:User)
+						WHERE me.id = $meId
+						OPTIONAL MATCH (me)-[rfollow:FOLLOW]->(p)
+						OPTIONAL MATCH (me)-[rlike:LIKE]-(e)
+						OPTIONAL MATCH (u:User)-[:LIKE]->(e)
+						WITH {
+							id:o.id,
+							dateTimeInit:o.dateTimeInit,
+							dateTimeEnd:o.dateTimeEnd,
+							tickets:collect(distinct t { .id, .price, .name, .amount, .description})
+						} as occ, e, collect(distinct tag { .title, .color, .description}) as tags, p, pl, cat, collect(distinct c {.id,.avatar,.username}) as coll,count(distinct u) as usersInterested, rlike,rfollow,me,item
+						with distinct {
+							id:e.id,
+							name:e.name,
+							occurrences:collect(occ),
+							info:e.description,
+							publisher:{
+								id:p.id,
+								avatar:p.avatar,
+								username:p.username,
+								businessName:p.businessName,
+								IFollowThis: rfollow IS NOT null
+							},
+							category:{
+								name:cat.name,
+								id:cat.id
+							},
+							place:{
+								name:pl.name,
+								address:pl.address,
+								longitude:(pl.longitude),
+								latitude:(pl.latitude)
+							},
+							collaborators: coll,
+							multimedia:e.multimedia,
+							attentionTags: tags,
+							amIInterested: rlike IS NOT null,
+							totalUsersInterested: usersInterested,
+							dateTimeInit:e.dateTimeInit,
+							dateTimeEnd:e.dateTimeEnd,
+							basePrice:e.basePrice
+						} as eventInfo, me, e,item
+						call {
+							WITH e,me
+							OPTIONAL MATCH (me)-[:FRIEND]-(f:User)-[:LIKE]->(e)
+							RETURN f LIMIT 3
+						}
+						WITH apoc.map.merge(eventInfo, {friends:collect( distinct f{.username, .avatar})}) as events,item
+						return {
+							type:"collection",
+							id:item.id,
+							name:item.name,
+							description:NULL,
+							events:collect(events),
+							showMini:true,
+							createdAt:item.createdAt
+						} as result'
+				],
+				'return null as result',
+				{item:p,meId:$meId}) YIELD value
 				return value.result as r
 				ORDER BY r.createdAt DESC
 			`,
@@ -158,6 +227,7 @@ export class PublicationsReadService {
         .limit(limit)
         .map((r) => {
           delete r.createdAt;
+          if (r.type === 'collection') console.log(r.name);
           return r.type === 'event'
             ? {
                 ...r,
