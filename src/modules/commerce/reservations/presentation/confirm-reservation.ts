e4e99efac4013bf00ab7f96896739b93d62f42ec
jvaldesgonzalez <jvaldesgonzalez9@gmail.com>
2022-02-22 18:46:45 -0500

    commit

--- src/modules/commerce/reservations/presentation/confirm-reservation.ts
+++ src/modules/commerce/reservations/presentation/confirm-reservation.ts
@@ -3,9 +3,12 @@ import { IsString, IsUUID } from 'class-validator';
 
 export class ConfirmReservationBody {
   @ApiProperty()
+  @IsString()
   securityPhrase: string;
   @ApiProperty()
+  @IsUUID(4)
   eventId: string;
   @ApiProperty()
+  @IsUUID(4)
   occurrenceId: string;
 }
