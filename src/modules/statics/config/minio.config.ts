29353160cc3b7af2d3fa71f46cc427806b89d2c1
jvaldesgonzalez <jvaldesgonzalez9@gmail.com>
2022-03-11 11:48:53 -0500

    build(statics): extract to .env credentials and host of minio

--- src/modules/statics/config/minio.config.ts
+++ src/modules/statics/config/minio.config.ts
@@ -1,14 +1,14 @@
 import { MinioOptions } from 'nestjs-minio-client/dist/interfaces/minio.options.interface';
 
 export const minioConfig: MinioOptions = {
-  endPoint: '152.206.177.203',
-  port: 9000,
+  endPoint: process.env.MINIO_HOST,
+  port: 80,
   useSSL: false,
-  accessKey: 'minio',
-  secretKey: 'hJG5myodnc9cc4!N',
+  accessKey: process.env.MINIO_USER,
+  secretKey: process.env.MINIO_PASS,
 };
 
 export const staticsBucketConfig = {
-  name: 'saturday.static',
+  name: 'media',
   ttl: 24 * 60 * 60, //1d
 };
