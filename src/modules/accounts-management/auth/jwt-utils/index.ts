e1f0ebe5ed4a18f091dc8956ed2acecdc83f5d80
jvaldesgonzalez <jvaldesgonzalez9@gmail.com>
2022-04-12 20:53:42 -0400

    fix(auth) set jwt exp to 30s

--- src/modules/accounts-management/auth/jwt-utils/index.ts
+++ src/modules/accounts-management/auth/jwt-utils/index.ts
@@ -5,7 +5,7 @@ import crypto from 'crypto';
 
 export namespace JWTUtils {
   export function sign(claim: JWTClaim) {
-    return jwt.sign(claim, jwtSecret, { expiresIn: '2h' });
+    return jwt.sign(claim, jwtSecret, { expiresIn: '30s' });
   }
 
   export function decode(token: JWTToken): JWTClaim {
